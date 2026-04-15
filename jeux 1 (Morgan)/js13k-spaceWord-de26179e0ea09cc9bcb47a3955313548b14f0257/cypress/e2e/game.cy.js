fd41c0907f55481fd0f24ea2c5b4e27326311280
Morgan Rehel <morganrehel@MacBook-Pro.local>
2026-04-15 16:59:17 +0200

    add cypress complet

---
+++ jeux 1 (Morgan)/js13k-spaceWord-de26179e0ea09cc9bcb47a3955313548b14f0257/cypress/e2e/game.cy.js
@@ -0,0 +1,33 @@
+describe('Tests Fonctionnels SpaceWord', () => {
+  
+  // ouvre la page du jeu
+  beforeEach(() => {
+    cy.visit('index.html')
+  })
+
+  it('Test 1 : Le menu principal s\'affiche avec le bouton PLAY', () => {
+    cy.get('nav').first().should('be.visible')
+    cy.get('#jogar-btn').should('contain', 'PLAY')
+  })
+
+  it('Test 2 : Le clic sur PLAY lance le jeu et masque le menu', () => {
+    cy.get('#jogar-btn').click()
+    
+    cy.get('nav').first().should('not.be.visible')
+    
+    cy.get('canvas#canvas').should('exist')
+  })
+
+  it('Test 3 : L\'interface de jeu (Chrono, Vies, Mot) s\'affiche en jouant', () => {
+    cy.get('#jogar-btn').click()
+    
+    // Vérifie que la barre de vie est bien générée par le JavaScript
+    cy.get('.lifebar').should('exist')
+    
+    // Vérifie que le chronomètre tourne et n'est pas vide
+    cy.get('.cronometer').should('not.be.empty')
+    
+    cy.get('.text').should('exist')
+  })
+
+})
