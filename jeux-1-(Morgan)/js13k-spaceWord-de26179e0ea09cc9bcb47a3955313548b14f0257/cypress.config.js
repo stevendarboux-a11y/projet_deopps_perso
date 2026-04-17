ed040cdb6c67fabc66e9335d49094fdd7c3c4c19
Morgan Rehel <morganrehel@MacBook-Pro.local>
2026-04-17 09:52:13 +0200

    fix error lint

--- jeux-1-(Morgan)/js13k-spaceWord-de26179e0ea09cc9bcb47a3955313548b14f0257/cypress.config.js
+++ jeux-1-(Morgan)/js13k-spaceWord-de26179e0ea09cc9bcb47a3955313548b14f0257/cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-    },
-    supportFile: false,
-  },
+	e2e: {
+		setupNodeEvents() {
+			// Callback vide
+		},
+		supportFile: false,
+	},
 });
